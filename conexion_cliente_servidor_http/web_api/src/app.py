ae87b4e23a25cc7a36ac04df12eaee6068a6f38f
ThePromidius <thepromidiusyt@gmail.com>
2023-11-22 19:20:34 +0100

    test

--- conexion_cliente_servidor_http/web_api/src/app.py
+++ conexion_cliente_servidor_http/web_api/src/app.py
@@ -4,7 +4,7 @@ from flask import Flask, render_template, redirect, url_for, request, make_respo
 
 import src.database as database
 app = Flask(__name__)
-from src import database as db
+# from src import database as db
 
 
 diccionario_puertas = {}
@@ -17,7 +17,7 @@ def actualizar_codigo_puerta(codigo_puerta: int):
     :param codigo_puerta:
     :return: El nuevo codigo
     """
-    puerta = database.session.query(db.Doors).filter(db.Doors.id_puerta == codigo_puerta).first()
+    puerta = database.session.query(database.Doors).filter(database.Doors.id_puerta == codigo_puerta).first()
 
     if not puerta:
         return "Invalid request", 400
@@ -30,7 +30,7 @@ def actualizar_codigo_puerta(codigo_puerta: int):
 @app.route('/')
 def main_page():
     # Query for all users in the database
-    users = database.session.query(db.Usuarios).all()
+    users = database.session.query(database.Usuarios).all()
 
     return render_template('main_page.html', users=users)
 
@@ -50,7 +50,7 @@ def list_users():
 @app.route('/add_user')
 def add_user():
     # Query for all doors in the database
-    doors = database.session.query(db.Doors).all()
+    doors = database.session.query(database.Doors).all()
 
     return render_template('add_user.html', doors=doors)
 
@@ -65,7 +65,7 @@ def api_add_user():
         return "Invalid request", 400
 
     # Create a new user with the data from the form
-    new_user = db.Usuarios(
+    new_user = database.Usuarios(
         rfid=rfid,
         nombre=nombre,
         apellidos=apellidos,
@@ -73,7 +73,7 @@ def api_add_user():
 
     # Assign the user to the selected doors
     for door_id in doors:
-        door = database.session.query(db.Doors).filter(db.Doors.id_puerta == door_id).first()
+        door = database.session.query(database.Doors).filter(database.Doors.id_puerta == door_id).first()
         new_user.puertas.append(door)
 
     # Add the user to the session
@@ -87,10 +87,10 @@ def api_add_user():
 @app.route('/edit_user/<rfid>')
 def edit_user(rfid):
     # Query for the user with the specified RFID
-    user = database.session.query(db.Usuarios).filter(db.Usuarios.rfid == rfid).first()
+    user = database.session.query(database.Usuarios).filter(database.Usuarios.rfid == rfid).first()
 
     # Query for all doors in the database
-    doors = database.session.query(db.Doors).all()
+    doors = database.session.query(database.Doors).all()
 
     return render_template('edit-user.html', user=user, doors=doors)
 
@@ -104,7 +104,7 @@ def api_edit_user():
     doors = request.form.getlist('doors')
 
     # Query for the user with the specified RFID
-    user = database.session.query(db.Usuarios).filter(db.Usuarios.rfid == rfid).first()
+    user = database.session.query(database.Usuarios).filter(database.Usuarios.rfid == rfid).first()
 
     # Update the user's information
     user.nombre = nombre
@@ -113,7 +113,7 @@ def api_edit_user():
     # Assign the user to the selected doors
     user.puertas = []
     for door_id in doors:
-        door = database.session.query(db.Doors).filter(db.Doors.id_puerta == door_id).first()
+        door = database.session.query(database.Doors).filter(database.Doors.id_puerta == door_id).first()
         user.puertas.append(door)
 
     # Commit the changes to the database
@@ -149,7 +149,7 @@ def save_door():
     nombre = request.form['nombre']
 
     # Create a new door with the data from the form
-    new_door = db.Doors(
+    new_door = database.Doors(
         nombre=nombre,
     )
 
@@ -174,7 +174,7 @@ def list_doors():
 @app.route('/edit_door/<id_puerta>')
 def edit_door(id_puerta):
     # Query for the door with the specified ID
-    door = database.session.query(db.Doors).filter(db.Doors.id_puerta == id_puerta).first()
+    door = database.session.query(database.Doors).filter(database.Doors.id_puerta == id_puerta).first()
 
     return render_template('edit-door.html', door=door)
 
@@ -184,15 +184,15 @@ def access_log():
     offset = request.args.get('offset', 0, type=int)
     limit = request.args.get('limit', 50, type=int)
 
-    access_logs = db.session.query(db.AccessLog, db.Usuarios) \
-        .join(db.Usuarios, db.AccessLog.rfid == db.Usuarios.rfid) \
+    access_logs = database.session.query(database.AccessLog, database.Usuarios) \
+        .join(database.Usuarios, database.AccessLog.rfid == database.Usuarios.rfid) \
         .offset(offset) \
         .limit(limit) \
         .with_entities(
-            db.AccessLog.id_puerta.label('id_puerta'),
-            db.AccessLog.rfid.label('rfid_usuario'),
-            db.AccessLog.timestamp.label('fecha_acceso'),
-            db.Usuarios.nombre.label('nombre')
+            database.AccessLog.id_puerta.label('id_puerta'),
+            database.AccessLog.rfid.label('rfid_usuario'),
+            database.AccessLog.timestamp.label('fecha_acceso'),
+            database.Usuarios.nombre.label('nombre')
         ) \
         .all()
 
@@ -214,7 +214,7 @@ def verificar_acceso():
         return "Invalid request", 400
 
     # Obtenemos los datos de la puerta
-    puerta: db.Doors = db.session.query(db.Doors).filter_by(id_puerta=puerta).first()
+    puerta: database.Doors = database.session.query(database.Doors).filter_by(id_puerta=puerta).first()
     # Obtenemos el codigo actual de esa puerta
     codigo_descifrado: float = puerta.codigo
 
@@ -237,7 +237,7 @@ def verificar_acceso():
 
 
     # Find the user with the given RFID in the "Usuarios" table
-    user = db.session.query(db.Usuarios).filter_by(rfid=rfid).first()
+    user = database.session.query(database.Usuarios).filter_by(rfid=rfid).first()
 
     # Si el usuario no se ha encontrado devuelve error
     if user is None:
@@ -247,11 +247,11 @@ def verificar_acceso():
     acceso_concedido = puerta in [door.id_puerta for door in user.puertas]
 
     # Create a new AccessLog object and set the "acceso_concedido" value
-    access_log = db.AccessLog(id_puerta=puerta, rfid=rfid, acceso_concedido=acceso_concedido)
+    access_log = database.AccessLog(id_puerta=puerta, rfid=rfid, acceso_concedido=acceso_concedido)
 
     # Add the access log to the database and commit the changes
-    db.session.add(access_log)
-    db.session.commit()
+    database.session.add(access_log)
+    database.session.commit()
     nuevo_codigo = actualizar_codigo_puerta(puerta)
 
     # Return a success or error message
@@ -268,7 +268,7 @@ def inicializar_puerta():
     else:
         return "Invalid request", 400
     # Find the user with the given RFID in the "Usuarios" table
-    puerta = db.session.query(db.Doors).filter_by(id_puerta=num_puerta).first()
+    puerta = database.session.query(database.Doors).filter_by(id_puerta=num_puerta).first()
     if not puerta:
         return "Error autentificacion", 403
     # Create a new code.
